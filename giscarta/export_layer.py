5c1a6e6c932e38cc2ad549fa7d01a447c7b9fa1c
giscarta <alex.k@giscarta.com>
2025-12-24 19:22:59 +0300

    update field types and raster import

--- giscarta/export_layer.py
+++ giscarta/export_layer.py
@@ -1,22 +1,25 @@
 from qgis.PyQt.QtWidgets import QFileDialog, QMessageBox
-from qgis.core import QgsProject, QgsVectorLayer, QgsFeature, QgsField, QgsGeometry, QgsPointXY
+from qgis.core import QgsProject, QgsVectorLayer, QgsFeature, QgsField, QgsGeometry, QgsPointXY, QgsRasterLayer
 from qgis.gui import *
 from qgis.PyQt.QtCore import QVariant
 import requests
+import tempfile
+import os
 
 def _export_layer(self): 
     selectedIndex = self.selected_rows_indexes[0]
     user_layer = self.user_layers[selectedIndex]
     username = self.username
     password = self.password
+    base_domain = self.domain
     endpoint = '/auth/token/login' 
     data = {'username': username, 'password': password}
     response = self.api.post(endpoint, data = data)
     self.token = response.get('auth_token')    
-    url = "https://map.giscarta.com/geoserver/user_data/ows" 
+    url = f"{base_domain}/geoserver/user_data/ows" 
 
     headers = {
-            "Referer": "https://map.giscarta.com/builder/",
+            "Referer": f"{base_domain}/builder/",
             "Authorization": f"Token {self.token}"}
 
     if user_layer['type'] != 'raster':
@@ -38,12 +41,39 @@ def _export_layer(self):
             geojson_data = response.json()
             geom_type = user_layer['geometryType'].lstrip('org.locationtech.jts.geom.')
             lyr = QgsVectorLayer(f'{geom_type}?crs=epsg:4326', str(user_layer['title']), 'memory')
-            field_names = list(geojson_data['features'][0]['properties'].keys())
-            fields = []
-            for field_name in field_names:
-                fields.append(QgsField(field_name, QVariant.String))
-            lyr.dataProvider().addAttributes(fields)
-            lyr.updateFields()
+            
+            def guess_qvariant_type(value):
+                if value is None:
+                    return QVariant.String
+                if isinstance(value, int):
+                    return QVariant.Int
+                if isinstance(value, float):
+                    return QVariant.Double
+                if isinstance(value, str):
+                    dt = QtCore.QDateTime.fromString(value, QtCore.Qt.ISODate)
+                    if dt.isValid():
+                        return QVariant.DateTime
+                    if value.lower() in ('true', 'false', 'yes', 'no', '1', '0'):
+                        return QVariant.Bool
+                    return QVariant.String
+                if isinstance(value, bool):
+                    return QVariant.Bool
+                if isinstance(value, (list, dict)):
+                    return QVariant.String
+                return QVariant.String
+            
+            if geojson_data['features'] and len(geojson_data['features']) > 0:
+                sample_props = geojson_data['features'][0]['properties']
+                fields = []
+                
+                for field_name, value in sample_props.items():
+                    qtype = guess_qvariant_type(value)
+                    fields.append(QgsField(field_name, qtype))
+                
+                lyr.dataProvider().addAttributes(fields)
+                lyr.updateFields()
+            else:
+                print("No features found in the layer")
 
             def create_geometry(geom_type, coordinates):
                 if geom_type == 'Point':
@@ -129,7 +159,41 @@ def _export_layer(self):
                 if valid_geom:
                     new_feature = QgsFeature()
                     new_feature.setGeometry(valid_geom)
-                    new_feature.setAttributes(list(properties.values()))
+                    
+                    attr_values = []
+                    for field in lyr.fields():
+                        field_name = field.name()
+                        value = properties.get(field_name)
+                        
+                        if value is None:
+                            attr_values.append(None)
+                            continue
+                            
+                        if field.type() == QVariant.Int:
+                            try:
+                                attr_values.append(int(value))
+                            except:
+                                attr_values.append(None)
+                        elif field.type() == QVariant.Double:
+                            try:
+                                attr_values.append(float(value))
+                            except:
+                                attr_values.append(None)
+                        elif field.type() == QVariant.Bool:
+                            if isinstance(value, str):
+                                attr_values.append(value.lower() in ('true', 'yes', '1'))
+                            else:
+                                attr_values.append(bool(value))
+                        elif field.type() == QVariant.DateTime:
+                            dt = QtCore.QDateTime.fromString(value, QtCore.Qt.ISODate)
+                            if dt.isValid():
+                                attr_values.append(dt)
+                            else:
+                                attr_values.append(None)
+                        else:
+                            attr_values.append(str(value))
+                    
+                    new_feature.setAttributes(attr_values)
                     features.append(new_feature)
                     valid_count += 1
                 else:
@@ -161,9 +225,72 @@ def _export_layer(self):
             msg.exec()
     
     else: # for raster 
+        try:
+            temp_dir = tempfile.gettempdir()
+            raster_filename = f"{user_layer['name']}.tif"
+            raster_path = os.path.join(temp_dir, raster_filename)
+            
+            wcs_url = f"{base_domain}/geoserver/wcs"
+            coverage_name = f"user_rasters:{user_layer['name']}"
+                        
+            try:
+                describe_params = {
+                    "service": "WCS",
+                    "version": "2.0.1",
+                    "request": "DescribeCoverage",
+                    "coverageId": coverage_name
+                }
+                
+                describe_response = requests.get(wcs_url, params=describe_params, headers=headers)                
+                if describe_response.status_code == 200:
+                    get_coverage_params = {
+                        "service": "WCS",
+                        "version": "2.0.1",
+                        "request": "GetCoverage",
+                        "coverageId": coverage_name,
+                        "format": "image/tiff"
+                    }
+                    
+                    response = requests.get(wcs_url, params=get_coverage_params, headers=headers, stream=True, timeout=30)
+                    if response.status_code == 200:
+                        with open(raster_path, 'wb') as f:
+                            for chunk in response.iter_content(chunk_size=8192):
+                                f.write(chunk)
+                        
+                        print(f"raster path: {raster_path}")
+                        
+                        layer_name = str(user_layer['title'])
+                        raster_layer = QgsRasterLayer(raster_path, layer_name)
+                        
+                        if raster_layer.isValid():
+                            QgsProject.instance().addMapLayer(raster_layer)
+                            
+                            msg = QMessageBox()
+                            msg.setIcon(QMessageBox.Information)
+                            msg.setText("Layer was loaded successfully")
+                            msg.setWindowTitle("Status")
+                            msg.setStandardButtons(QMessageBox.Ok)
+                            returnValue = msg.exec()
+                        else:
+                            raise Exception("Invalid raster layer file")
+                    else:
+                        raise Exception(f"WCS GetCoverage failed: {response.status_code}")
+                else:
+                    raise Exception(f"Coverage not found with WCS 2.0.1")
+                        
+            except Exception as e:
+                print(f"Raster download error: {str(e)}")
+                msg = QMessageBox()
+                msg.setIcon(QMessageBox.Warning)
+                msg.setText(f"Raster layer loading error: {str(e)}")
+                msg.setWindowTitle("Error")
+                msg.setStandardButtons(QMessageBox.Ok)
+                returnValue = msg.exec()
+        except Exception as e:
+            print(f"Raster download error: {str(e)}")
             msg = QMessageBox()
             msg.setIcon(QMessageBox.Warning)
-            msg.setText("Trying to load a raster layer")
+            msg.setText(f"Raster layer loading error: {str(e)}")
             msg.setWindowTitle("Error")
             msg.setStandardButtons(QMessageBox.Ok)
             returnValue = msg.exec()
@@ -172,7 +299,7 @@ def _export_layer(self):
 def _export_click(self):
     msg = QMessageBox()
     msg.setIcon(QMessageBox.Question)
-    msg.setText("Are you sure you want to import a layer from GISCARTA?")
+    msg.setText("Are you sure you want to download a layer from GISCARTA?")
     msg.setWindowTitle("Saving a layer")
     msg.setStandardButtons(QMessageBox.Ok | QMessageBox.Cancel)
     returnValue = msg.exec()
